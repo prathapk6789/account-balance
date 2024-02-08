57be0a8dc8a0cc46bb124cf923188b24d4f0a6d6
k.Prathap <159271537+prathapk6789@users.noreply.github.com>
2024-02-08 20:26:37 +0530

    marks

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-amount=1000
-debit=50
-print(amount-debit)
-credit=20
-print(amount-debit+credit)
+telugu=80
+english=90
+maths=70
+science=80
+hindi=80
+social=90
+total_score=600
+total_marks=telugu+english+maths+science+hindi+social
+total_percentage=total_marks/total_score*100
+print(total_marks)
+print(total_percentage)
+print(f"prathap scored {490} with={490/6}%")
